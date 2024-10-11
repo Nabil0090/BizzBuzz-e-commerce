1020b1ef00f674fc10e2387f2a00a539e61ce437
Nabil0090 <rafatnabil60@gmail.com>
2024-10-11 21:04:41 +0600

    Initial commit

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',          
   user: 'root',             
-  password: 'password',       
+  password: 'R@fat2014',       
   database: 'BIZZBUZZ'        
 });
 
